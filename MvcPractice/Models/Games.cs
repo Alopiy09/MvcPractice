9290120c5e6982c79aa1438e1205da040157c82a
jacob sanders <jkehsanders@gmail.com>
2019-07-21 17:57:46 -0500

    added games to project

---
+++ MvcPractice/Models/Games.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcPractice.Models
+{
+    public class Games
+    {
+        [key]
+        public int GameId { get; set; }
+    }
+}
